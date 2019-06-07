cf6ba552c1e546641ee489180486c861a62cebd7
Patrick Rahner <patrick.rahner@inotech.eu>
2019-06-07 09:08:11 +0200

    Help hinzugefügt

---
+++ Help/DiagramDesignerPart1/MoveResizeRotateWithAdorners/ToolboxItem.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace DiagramDesigner
+{
+    public class ToolboxItem : ContentControl
+    {
+        public ToolboxItem()
+        {
+            try
+            {
+                this.Resources.MergedDictionaries.Add(new ResourceDictionary() { Source = new Uri("pack://application:,,,/MoveResizeRotateWithAdorners;component/Resources/DesignerItem.xaml", UriKind.RelativeOrAbsolute) });
+                Style = FindResource("DesignerItemStyle") as Style;
+            }
+            catch (Exception ex)
+            {
+            }
+        }
+    }
+}
